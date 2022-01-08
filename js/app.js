c22e168637ae9bbab00de7c6b23878cb0c82a010
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-01-08 17:38:38 +0700

    feat: implmenent countdown timer and stop it

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const minutesElement = document.querySelector(".timer .minutes input");
+const secondsElement = document.querySelector(".timer .seconds input");
+const startButton = document.querySelector(".start");
+const settingsButton = document.querySelector(".settings");
+
+let timer;
+
+startButton.addEventListener("click", function () {
+  let isStarting = (this.textContent =
+    this.textContent.toLowerCase() === "start");
+  this.textContent = isStarting ? "pause" : "start";
+  if (isStarting) {
+    const minutes = Number(minutesElement.value);
+    const seconds = Number(secondsElement.value);
+    let countdown = minutes * 60 + seconds;
+
+    timer = setInterval(() => {
+      let hours = Math.floor(countdown / 3600); // get hours
+      let m = Math.floor((countdown - hours * 3600) / 60); // get minutes
+      let s = countdown - hours * 3600 - m * 60; //  get seconds
+
+      minutesElement.value = ("0" + m).slice(-2);
+      ("0" + s).slice(-2);
+      secondsElement.value = ("0" + s).slice(-2);
+
+      countdown--;
+      if (countdown < 0) {
+        clearInterval(timer);
+      }
+    }, 1000);
+  } else {
+    clearInterval(timer);
+  }
+});
